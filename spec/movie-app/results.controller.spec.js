7d9120660ccb07d5cd46cfa07aa6ec85df896e52
Kris Heinrich <krisheinrich@gmail.com>
2016-10-09 14:59:40 -0700

    Initial commit

--- spec/movie-app/results.controller.spec.js
+++ spec/movie-app/results.controller.spec.js
@@ -28,19 +28,27 @@ describe('Results Controller', function () {
       $q,
       $rootScope,
       $scope,
+      $exceptionHandler,
       omdbApi;
 
   beforeEach(module('omdb'));
   beforeEach(module('movieApp'));
 
-  beforeEach(inject(function (_$controller_, _$location_, _$q_, _$rootScope_, _omdbApi_) {
+  beforeEach(module(function ($exceptionHandlerProvider) {
+    $exceptionHandlerProvider.mode('log');
+  }));
+
+  beforeEach(inject(function (_$controller_, _$location_, _$q_, _$rootScope_, _$exceptionHandler_, _omdbApi_) {
     $controller = _$controller_;
     $location = _$location_;
     $q = _$q_;
     $scope = {};
     $rootScope = _$rootScope_;
+    $exceptionHandler = _$exceptionHandler_;
     omdbApi = _omdbApi_;
   }));
+
+  /******************************** TESTS ********************************/
 
   it('should load search results', function () {
     spyOn(omdbApi, 'search').and.callFake(function () {
@@ -61,14 +69,14 @@ describe('Results Controller', function () {
   it('should set results status to error', function () {
     spyOn(omdbApi, 'search').and.callFake(function () {
       var deferred = $q.defer();
-      deferred.reject();
+      deferred.reject('Something went wrong!');
       return deferred.promise;
     });
     $location.search('q', 'star wars');
     $controller('ResultsController', { $scope: $scope });
     $rootScope.$apply();
 
-    expect($scope.errorMessage).toBe('Something went wrong!');
+    expect($exceptionHandler.errors).toEqual(['Something went wrong!']);
   });
 
 
