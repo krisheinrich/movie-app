9fa72602fbe32bd0841348cbd36d50c88e881461
Kris Heinrich <krisheinrich@gmail.com>
2016-10-09 15:06:50 -0700

    Revert "Revert "Initial commit""
    This reverts commit e592740adad012b7d85c6fb321b362f0fbf20220.

--- movie-app/app.js
+++ movie-app/app.js
@@ -1,6 +1,10 @@
-angular.module('movieApp', ['ui.bootstrap', 'ngRoute', 'omdb'])
+angular.module('movieApp', ['ui.bootstrap', 'ngRoute', 'omdb', 'movieCore', 'ngMockE2E'])
   .config(function ($routeProvider) {
     $routeProvider
+      .when('/', {
+        templateUrl: 'movie-app/home.html',
+        controller: 'HomeController'
+      })
       .when('/results', {
         templateUrl: 'movie-app/results.html',
         controller: 'ResultsController'
@@ -8,4 +12,17 @@ angular.module('movieApp', ['ui.bootstrap', 'ngRoute', 'omdb'])
       .otherwise({
         redirectTo: '/'
       });
+  })
+  .run(function ($httpBackend) {
+    var data = ['tt0076759', 'tt0080684', 'tt0086190'],
+        headers = {
+          headers: {'Content-Type': 'application/json'}
+        };
+
+        // Return Popular Movie IDs
+        $httpBackend.whenGET(function (s) {
+          return (s.indexOf('popular') !== -1);
+        }).respond(200, data, headers);
+
+        $httpBackend.whenGET(/.*/).passThrough();
   });
